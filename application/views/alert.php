9c303984c80fbc453b09797e963b94d700b5a300
Ardi <mhmmdard01@gmail.com>
2023-05-30 16:39:57 +0700

    Update: Alert;

--- application/views/alert.php
+++ application/views/alert.php
@@ -1,31 +1,16 @@
-<script src="<?php echo base_url('assets/admin-page/js/jquery.min.js'); ?>"></script>
-<link href="<?php echo base_url('assets/admin-page/css/font-awesome.min.css'); ?>" rel="stylesheet" />
-<script src="<?php echo base_url('assets/admin-page/js/toastr.min.js'); ?>"></script>
-<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/admin-page/css/toastr.min.css'); ?>">
+<link href="<?php echo base_url('assets/plugins/fontawesome-free/all.min.css'); ?>" rel="stylesheet" />
+<link rel="stylesheet" href="<?php echo base_url('assets/plugins/toastr/toastr.min.css'); ?>">
+<script src="<?php echo base_url('assets/plugins/jquery/jquery.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/plugins/toastr/toastr.min.js'); ?>"></script>
 
+<script type="text/javascript">
     <?php if ($this->session->flashdata('success')) { ?>
-        <!-- toastr.success("<?php echo $this->session->flashdata('success'); ?>"); -->
+        toastr.success("<?php echo $this->session->flashdata('success'); ?>");
     <?php } else if ($this->session->flashdata('error') || validation_errors()) {  ?>
-        <div className="-mb-20 justify-center items-center justify-items-center transition-all duration-200">
-            <div className="border-2 border-red-light rounded-xl w-464 h-20 text-red-light bg-red-smooth flex">
-                <div className="m-auto w-full">
-                    <div className="font-semibold text-xl pl-4 mdmax:text-smallest">
-                        <?php echo (validation_errors()) ? json_encode(validation_errors()) : json_encode($this->session->flashdata('error')); ?>
-                    </div>
-                </div>
-            </div>
-        </div>
+        toastr.error(<?php echo (validation_errors()) ? json_encode(validation_errors()) : json_encode($this->session->flashdata('error')); ?>);
     <?php } else if ($this->session->flashdata('warning')) {  ?>
-        <div className="-mb-20 justify-center items-center justify-items-center transition-all duration-200">
-            <div className="border-2 border-red-light rounded-xl w-464 h-20 text-red-light bg-red-smooth flex">
-                <div className="m-auto w-full">
-                    <div className="font-semibold text-xl pl-4 mdmax:text-smallest">
-                        <?php echo $this->session->flashdata('warning'); ?>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- toastr.warning(""); -->
+        toastr.warning("<?php echo $this->session->flashdata('warning'); ?>");
     <?php } else if ($this->session->flashdata('info')) {  ?>
-        <!-- toastr.info("<?php echo $this->session->flashdata('info'); ?>"); -->
+        toastr.info("<?php echo $this->session->flashdata('info'); ?>");
     <?php } ?>
+</script>
